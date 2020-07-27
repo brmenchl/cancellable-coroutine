6952fbf6ec67e75d4bf3c7709c00a81c9c57d8cb
Brad Menchl <brmenchl@gmail.com>
2020-07-27 14:02:35 -0400

    Initial with tsdx

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+const CANCEL = Symbol("CANCEL");
+const IS_CANCELLED = Symbol("IS_CANCELLED");
+
+class CancelError extends Error {
+  constructor(message?: string) {
+    super(message); // 'Error' breaks prototype chain here
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    this.name = "CancelError";
+  }
+}
+
+type CancellableTask<Args extends ReadonlyArray<unknown>> = {
+  (...args: Args): Promise<any>;
+  [CANCEL](): void;
+  [IS_CANCELLED](): boolean;
+};
+
+export const create = <F extends (...args: any[]) => Generator>(fn: F) => {
+  const cancellableTask = ((async (...args: Parameters<F>) => {
+    const state = {
+      isRunning: true,
+      isCancelled: false
+    };
+    const { it, result } = runner(fn, ...args);
+
+    cancellableTask[CANCEL] = () => {
+      if (state.isRunning) {
+        const err = new CancelError();
+        state.isCancelled = true;
+        it.throw(err);
+      }
+    };
+
+    cancellableTask[IS_CANCELLED] = () => state.isCancelled;
+
+    const completion = await result;
+    state.isRunning = false;
+    return completion;
+  }) as unknown) as CancellableTask<Parameters<F>>;
+
+  return cancellableTask;
+};
+
+export const cancel = (...tasks: CancellableTask<any>[]) => {
+  tasks.forEach(task => task[CANCEL]());
+};
+
+export const isCancelled = (task: CancellableTask<any>) => task[IS_CANCELLED]();
+
+export const isCancelError = (error: Error): error is CancelError =>
+  error instanceof CancelError;
+
+const runner = <F extends (...args: any[]) => Generator>(
+  gen: F,
+  ...args: Parameters<F>
+) => {
+  const it = gen(...args);
+
+  const step = async (verb: "next" | "throw", arg?: any): Promise<any> => {
+    try {
+      const result = it[verb](arg);
+      if (result.done) {
+        return await result.value;
+      }
+      const value = await result.value;
+      if (value instanceof Error) {
+        throw value;
+      } else {
+        return step("next", value);
+      }
+    } catch (err) {
+      return step("throw", err);
+    }
+  };
+
+  return { it, result: step("next") };
+};
---
+++ test/index.test.ts
@@ -0,0 +1,90 @@
+import * as Cancellable from '../src';
+
+describe('Cancellable', () => {
+  it('should run', async () => {
+    const afterPromise = jest.fn();
+
+    const gen = function*() {
+      const sum = yield 1 + 1;
+      yield new Promise(res => setTimeout(res, 50));
+      yield afterPromise(sum);
+      expect(afterPromise).toBeCalledWith(2);
+      return sum;
+    };
+
+    const returnValue = await Cancellable.create(gen)();
+    expect(returnValue).toEqual(2);
+  });
+
+  it('should cancel on yield Error', async () => {
+    const afterError = jest.fn();
+    const inCatch = jest.fn();
+
+    const gen = function*() {
+      try {
+        yield new Error('Whoops');
+        yield afterError();
+      } catch (e) {
+        inCatch();
+        expect(Cancellable.isCancelError(e)).toEqual(false);
+        return e;
+      }
+    };
+
+    const cancellableTask = Cancellable.create(gen);
+    const promise = cancellableTask();
+    const returnValue = await promise;
+    expect(returnValue).toEqual(new Error('Whoops'));
+    expect(Cancellable.isCancelled(cancellableTask)).toEqual(false);
+    expect(inCatch).toHaveBeenCalled();
+    expect(afterError).not.toHaveBeenCalled();
+  });
+
+  it('should cancel on yield rejected Promise', async () => {
+    const afterRejectedPromise = jest.fn();
+    const inCatch = jest.fn();
+
+    const gen = function*() {
+      try {
+        yield Promise.reject('Whoops');
+        yield afterRejectedPromise();
+      } catch (e) {
+        inCatch();
+        expect(Cancellable.isCancelError(e)).toEqual(false);
+        return e;
+      }
+    };
+
+    const cancellableTask = Cancellable.create(gen);
+    const promise = cancellableTask();
+    const returnValue = await promise;
+    expect(returnValue).toEqual('Whoops');
+    expect(Cancellable.isCancelled(cancellableTask)).toEqual(false);
+    expect(inCatch).toHaveBeenCalled();
+    expect(afterRejectedPromise).not.toHaveBeenCalled();
+  });
+
+  it('should cancel on task cancel call', async () => {
+    const afterDelay = jest.fn();
+    const inCatch = jest.fn();
+
+    const gen = function*() {
+      try {
+        yield new Promise(res => setTimeout(res, 100));
+        yield afterDelay();
+      } catch (e) {
+        inCatch();
+        expect(Cancellable.isCancelError(e)).toEqual(true);
+        return e;
+      }
+    };
+
+    const cancellableTask = Cancellable.create(gen);
+    const promise = cancellableTask();
+    Cancellable.cancel(cancellableTask);
+    await promise;
+    expect(Cancellable.isCancelled(cancellableTask)).toEqual(true);
+    expect(inCatch).toHaveBeenCalled();
+    expect(afterDelay).not.toHaveBeenCalled();
+  });
+});
